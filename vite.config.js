37baa746e88aee195c7adce0a1e3287b296a2cbc
mehulsirvi <mhlsirvi@gmail.com>
2024-11-07 22:33:38 +0530

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/taskmanager/', // Replace with your repo name
+  plugins: [react()]
 })
