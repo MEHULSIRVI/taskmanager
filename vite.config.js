e96c044fb2ff85354ad3e0af7e3b2803f0fe7863
mehulsirvi <mhlsirvi@gmail.com>
2024-11-07 22:40:35 +0530

    resolved merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base: '/taskmanager/', // Replace with your repo name
   plugins: [react()]
-=======
-  base: '/taskmanager/',
-  plugins: [react()],
->>>>>>> 1b25396e1ee11962c8c03c6452408af50a88e78f
 })
